12e20a3c0629691d988985a76927bca3b9651f70
MalshaDissanayake <dmkmd333@gmail.com>
2024-07-30 14:49:37 +0530

    Add files for task 3.

---
+++ 3.Python_Programming-Basics/quiz_2.py
@@ -0,0 +1,16 @@
+#Suppose you are writing a program to capture the information of a student with the following details.
+
+#Name: Saman
+#Age: Thirteen years
+#Height: One point five meters
+#Address: 112A, Galle Road, Mount Lavinia.
+
+
+#Assign the above details to following four variables with the correct data type. For example, age should be a number with no decimal points.
+
+#Edit the following lines
+
+Name="Saman"
+Age=13
+Height=1.5
+Address="112A, Galle Road, Mount Lavinia"
