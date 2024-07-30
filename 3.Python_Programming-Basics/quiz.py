0dfe84f9834f1172367d4cacd1f6996dff55bfa2
MalshaDissanayake <dmkmd333@gmail.com>
2024-07-30 14:01:36 +0530

    Ad files for task 3.

---
+++ 3.Python_Programming-Basics/quiz.py
@@ -0,0 +1,18 @@
+#Write a Python program that takes the user's name as input and displays and welcomes them.
+#Expected behaviour: 
+
+#Enter your name: Nimal
+#Welcome Nimal
+
+#The Python code for taking the input and displaying the output is already provided.
+
+#take the user's name as input
+name = input("Enter your name: ")
+print(name)
+
+#the variable that includes the welcome message is already provided.
+#Please complete this part using the knowledge obtained in this lesson.
+greeting = "Welcome" + " " + name
+
+#print the welcome message
+print(greeting)
